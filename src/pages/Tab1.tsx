b4a2b509d4f5fe208a5a6a68dfefc8770b943f7d
phtalreja <51252603+phtalreja@users.noreply.github.com>
2020-08-08 16:00:54 -0700

    added sample array to my matches page

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonText, IonAvatar, IonLabel, IonItemOptions, IonItemSliding, IonItemOption } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+
+const arr = [
+  {
+    name: 'Hoang',
+    desc: 'I love all things design!',
+    pfp: "https://ca.slack-edge.com/T0171J44GNS-U0187P2F5T4-b1fd59458e4a-512"
+  },
+  {
+    name: 'Priya',
+    desc: 'Looking for a coding buddy :)',
+    pfp: "https://ca.slack-edge.com/T0171J44GNS-U01817EKK6E-a04444b7a86c-512"
+  },
+  {
+    name: 'Alex',
+    desc: 'Hello :)',
+    pfp: "https://ca.slack-edge.com/T0171J44GNS-U0183UWCZKM-gd3d946da65b-512"
+  }
+]
 
 const Tab1: React.FC = () => {
   return (
@@ -12,12 +30,27 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonList>
+          {arr.map(elem => (
+            <IonItemSliding key={elem.name}>
+              <IonItem>
+                <IonAvatar slot="start">
+                <img src={elem.pfp} />
+                </IonAvatar>
+                <IonLabel>
+                    <h2>{elem.name}</h2>
+                    <h3>{elem.desc}</h3>
+                </IonLabel>
+                </IonItem>
+                <IonItemOptions side="start">
+                    <IonItemOption>
+                      Chat
+                    </IonItemOption>
+                </IonItemOptions>
+            </IonItemSliding>
+          ))}
+
+        </IonList>
       </IonContent>
     </IonPage>
   );
