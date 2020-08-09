33ebd59ed82d769b00f61149f2b09fc206bd3574
phtalreja <51252603+phtalreja@users.noreply.github.com>
2020-08-08 17:02:27 -0700

    added avatar and name to settings page

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonLabel, IonList, IonItem, IonAvatar, IonCol, IonRow } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
@@ -10,29 +10,65 @@ const arr = [
     gender: 'female',
     friend_gender: 'female',
     interest: 'coding',
-    available: ''
-    
+    available: 'aug 8th @ 9AM-7PM'
   }
 ]
 
 const Tab3: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>settings</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
+    <>
+      <IonPage>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle>settings</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
-      </IonContent>
-    </IonPage>
+        <IonContent>
+    
+                  <IonAvatar>
+                    <img src="https://ca.slack-edge.com/T0171J44GNS-U0187P2F5T4-b1fd59458e4a-512" />
+                  </IonAvatar>
+                  <IonLabel>
+                      <h2>Hoang</h2>
+                  </IonLabel>
+        
+          <IonList>
+              {arr.map(elem => (
+                <>
+                  <IonItem>
+                    <IonLabel>
+                        My gender is {elem.gender}
+                    </IonLabel>
+                  </IonItem>
+
+                  <IonItem>
+                    <IonLabel>
+                        I'm looking for a friend that's preferably {elem.friend_gender}
+                    </IonLabel>
+                  </IonItem>
+
+                  <IonItem>
+                    <IonLabel>
+                        I am interested in {elem.interest}
+                    </IonLabel>
+                  </IonItem>
+
+                  <IonItem>
+                    <IonLabel>
+                        I'm available {elem.available}
+                    </IonLabel>
+                  </IonItem>
+
+                </>
+              
+              ))}
+          </IonList>
+        </IonContent>
+      </IonPage>
+    </>
   );
+  
 };
 
 export default Tab3;
+
