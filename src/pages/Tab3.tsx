566be32fdca9c8c6e063f1b06ab4c4de0acaeeab
phtalreja <51252603+phtalreja@users.noreply.github.com>
2020-08-08 20:59:47 -0700

    parsed avatar photo and name from array

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonLabel, IonList, IonItem, IonAvatar, IonCol, IonRow, IonModal, IonButton, IonIcon } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
-import { chevronForwardOutline } from 'ionicons/icons';
-
+import { chevronForwardOutline, arrowBackOutline } from 'ionicons/icons';
 const arr = [
   {
     name: 'Hoang',
@@ -11,40 +10,42 @@ const arr = [
     gender: 'female',
     friend_gender: 'female',
     interest: 'coding',
-    available: 'aug 8th @ 9AM-7PM'
+    available: 'aug 8th @ 9am-7pm'
   }
 ]
-
 const Tab3: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
-    <>
+    
       <IonPage>
+        
         <IonHeader>
           <IonToolbar>
             <IonTitle>settings</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent className="ion-padding">
-    
+          {arr.map(elem => (
+                
+                  <div>
                   <IonAvatar className="avatar">
-                    <img src="https://ca.slack-edge.com/T0171J44GNS-U0187P2F5T4-b1fd59458e4a-512" />
+                    <img src={elem.pfp} />
                   </IonAvatar>
                   <IonLabel>
-                      <h1 className="ion-text-center ion-padding">Hoang</h1>
+                      <h1 className="ion-text-center ion-padding">{elem.name}</h1>
                   </IonLabel>
-        
+
+
           <IonList className="ion-padding">
-              {arr.map(elem => (
-                <>
+              
                   <IonItem>
                     <IonLabel>
                         My gender is {elem.gender}
                     </IonLabel>
-                    <IonButton color="clear">
+                    <IonButton color="clear" onClick={() => setShowModal(true)}>
                       <IonIcon icon={chevronForwardOutline} />
                     </IonButton>
                   </IonItem>
-
                   <IonItem>
                     <IonLabel>
                         I'm looking for a friend that's preferably {elem.friend_gender}
@@ -53,7 +54,6 @@ const Tab3: React.FC = () => {
                       <IonIcon icon={chevronForwardOutline} />
                     </IonButton>
                   </IonItem>
-
                   <IonItem>
                     <IonLabel>
                         I am interested in {elem.interest}
@@ -62,7 +62,6 @@ const Tab3: React.FC = () => {
                       <IonIcon icon={chevronForwardOutline} />
                     </IonButton>
                   </IonItem>
-
                   <IonItem>
                     <IonLabel>
                         I'm available {elem.available}
@@ -71,32 +70,32 @@ const Tab3: React.FC = () => {
                       <IonIcon icon={chevronForwardOutline} />
                     </IonButton>
                   </IonItem>
-
-                </>
-              
+                  
+                  </IonList>
+                  </div>
               ))}
-          </IonList>
+          
+          
+          
+            <IonModal isOpen={showModal} cssClass='my-custom-class'>
+                <IonHeader>
+                  <IonToolbar>
+                    <IonTitle>settings</IonTitle>
+                      <IonButton color="clear" onClick={() => setShowModal(false)}>
+                        <IonIcon icon={arrowBackOutline} />
+                    </IonButton>
+                  </IonToolbar>
+              </IonHeader>
+              <IonContent className="ion-padding">
+                <h3 className="ion-text-center">
+                  I identify as ...
+                </h3>
+              </IonContent>
+          </IonModal>
         </IonContent>
       </IonPage>
-    </>
+      
   );
   
 };
-
 export default Tab3;
-
-export const ModalExample: React.FC = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  return (
-    <IonContent>
-      <IonLabel>Helloooo</IonLabel>
-      <IonModal isOpen={showModal} cssClass='my-custom-class'>
-        <p>This is modal content</p>
-        <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton>
-      </IonModal>
-      <IonButton onClick={() => setShowModal(true)}>Show Modal</IonButton>
-    </IonContent>
-  );
-};
-
